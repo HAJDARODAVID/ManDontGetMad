9a1654895fcd3bc076c8aee229d97d6660e75f96
David Hajdarovic <david.hajdarovic@boxmark.com>
2023-10-23 10:29:55 +0200

    minor fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -171,11 +171,12 @@
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         @yield('content')
+        
     </main>
   </div>
 </div>
 
-
+    @livewireScripts
     <script src="{{ url('js/bootstrap.bundle.min.js') }}"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
